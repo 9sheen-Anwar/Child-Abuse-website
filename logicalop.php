82d39b99919f7534b65bf7f4649470b8a6a71216
Nausheen Anwar <nausheenanwar39@gmail.com>
2021-03-13 12:29:59 +0530

    first commit

---
+++ logicalop.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<?php
+$a = 42, $b = 0;
+if( $a && $b ){
+echo "TEST1 : Both a and b are true<br/>";
+}else{
+echo "TEST1 : Either a or b is false<br/>";
+}
+if( $a and $b ){
+echo "TEST2 : Both a and b are true<br/>";
+}else{
+echo "TEST2 : Either a or b is false<br/>";
+}
+if( $a || $b ){
+echo "TEST3 : Either a or b is true<br/>";
+}else{
+echo "TEST3 : Both a and b are false<br/>";
+}
+if( $a or $b ){
+echo "TEST4 : Either a or b is true<br/>";
+}else{
+echo "TEST4 : Both a and b are false<br/>";
+}
+?>
+</body>
+</html>
